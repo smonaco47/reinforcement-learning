436d286f51c344536dab1b1a0073283c1f98245a
codemonkey047@gmail.com <codemonkey047@gmail.com>
2021-02-06 13:26:02 -0500

    Reorganize files

--- agents/grid_search.py
+++ agents/grid_search.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from tensorforce import Environment
 
 from agents.hyperparameters import Hyperparameters
-from agents.tensorforce.lunar_lander import train_model
+from agents.tensorforce_agent import train_model
 from agents.results import Results
 
 params = Hyperparameters()
@@ -16,9 +16,12 @@ iterations = 10000
 
 with open(f"output-{datetime.now().strftime('%H-%M-%S')}.txt", 'w') as out_file:
     out_file.write(f"{Hyperparameters.csv_header()},{Results.csv_header(iterations)}\n")
+
     for i in range(100):
         print(f"{datetime.now().strftime('%H:%M:%S')}\t{i}")
+
         params.randomize()
         result = train_model(lander_env, params, max_iterations=iterations, show_final=False)
         out_file.write(f"{params.csv_params()},{result.csv_result()}\n")
+
     lander_env.close()
